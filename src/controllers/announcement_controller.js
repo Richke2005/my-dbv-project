d27b63a5871c74962887aef7011c5235091c08ec
Richke2005 <richardke18@gmail.com>
2024-02-11 22:12:25 -0300

    add post route

--- src/controllers/announcement_controller.js
+++ src/controllers/announcement_controller.js
@@ -9,6 +9,25 @@ class AnnouncementController{
             next(error);
         }
     }
+ //TODO: announcement search
+ 
+    // static async getAnnoncementBySearch(req, res, next){
+	// 	try{
+	// 		const parameters = req.query;
+    //        
+	// 		const search = Search(parameters);
+
+	// 		if(search){
+	// 			const apiSearch = await announcement.find(search);
+	// 			res.status(200).send(apiSearch);
+	// 		}else{
+	// 			res.status(200).send([]);
+	// 		}
+
+	// 	}catch(error){
+	// 		next(error);
+	// 	};
+	// }
 
     static async getAnnouncementById(req, res, next){
         try{
@@ -23,6 +42,47 @@ class AnnouncementController{
             next(error);
         }
     }
+
+    static async postAnnouncement(req, res, next){
+		try{
+			const announcementData = new announcement(req.body);
+			await announcementData.save();
+
+			res.status(201).send({message: `${announcementData} created successfully`});
+		}catch(error){
+			next(error)
+		}
+	}
+
+	static async putAnnouncement(req, res, next){
+		try{
+			const id = req.params.id;
+			const updatedAnouncement = await announcement.findByIdAndUpdate(id, {$set: req.body});
+			if(updatedAnouncement !== null){
+				res.status(200).send({message: `${updatedAnouncement} updated successfully`});
+			}else{
+				res.status(401).send({message: `Announcement wih id ${id} was not found`});
+			}
+		}catch(error){
+			next(error)
+		}
+        
+	}
+
+	static async deleteAnnouncement(req, res, next){
+		try{
+			const id = req.params.id;
+
+			const deletedAnouncement = await announcement.findByIdAndDelete(id);
+			if(deletedAnouncement !== null){
+				res.status(200).send({message: `${deletedAnouncement} deleted successfully`});
+			}else{
+				res.status(401).send({message: `Announcement with id ${id} was not found`});
+			}
+		}catch(error){
+			next(error)
+		}
+	}
 }
 
 module.exports = AnnouncementController;
--- src/routes/announcement.js
+++ src/routes/announcement.js
@@ -4,6 +4,7 @@ const { AnnouncementController } = require("../controllers/");
 const router = Router();
 
 router.get("/announcement", AnnouncementController.getAnnouncement)
-.get("/announcement/:id", AnnouncementController.getAnnouncementById);
+.get("/announcement/:id", AnnouncementController.getAnnouncementById)
+.post("/announcement", AnnouncementController.postAnnouncement);
 
 module.exports = router;
