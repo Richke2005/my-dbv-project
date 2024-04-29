3f8a96398f1d5c33edf9551b2be38f8247af7499
Richke2005 <richardke18@gmail.com>
2024-04-29 01:04:51 -0300

    add a class of dayly verse

--- src/infra/services/bible_services.js
+++ src/infra/services/bible_services.js
@@ -1,18 +1,90 @@
-import axios from "axios";
+// import axios from "axios";
 
-const connAPI = axios.create({ baseURL: "https://www.abibliadigital.com.br/api" });
+class BibleServices{
+	#API = "https://www.abibliadigital.com.br/api"
+	#bibleVersion = "nvi";
+	#VERSE_INDEX = new Date().getDate();
+	#DAYLY_VERSES = [
+		`sl/23/2`,
+		`1pe/4/1`,
+		`1co/4/4`,
+		`gn/1/1`,
+		`rm/12/21`,
+		`sl/41/1`,
+		`lc/4/16`,
+		`pv/16/3`,
+		`sl/31/1`,
+		`pv/19/17`,
+		`sl/23/2`,
+		`1pe/4/1`,
+		`1co/4/4`,
+		`gn/1/1`,
+		`rm/12/21`,
+		`sl/41/1`,
+		`lc/4/16`,
+		`pv/16/3`,
+		`sl/31/1`,
+		`pv/19/17`,
+		`sl/23/2`,
+		`1pe/4/1`,
+		`1co/4/4`,
+		`gn/1/1`,
+		`rm/12/21`,
+		`sl/41/1`,
+		`lc/4/16`,
+		`pv/16/3`,
+		`sl/31/1`,
+		`pv/19/17`
+	]
+	
+	constructor(bibleVersion){
+		this.#bibleVersion = bibleVersion;
+	}
 
-async function getRandomVerse(version) {
-	const response = await connAPI.get(`/verses/${version}/random`);
-	return response.data;
+	async getVerseOfDay(){
+			const verseId = this.#DAYLY_VERSES[this.#VERSE_INDEX - 1];
+			const response = await fetch(`${this.#API}/verses/nvi/${verseId}`, {
+				method: "GET", 
+				mode: "no-cors",
+				referrerPolicy: "no-referrer",
+				headers: {
+					"Content-Type": "application/json",
+				}
+			});
+		const data = await response.json();
+		return data;
+	}
+
+	async getRandomVerse() {
+		const response = await fetch(`${this.#API}/verses/${this.#bibleVersion}/random`, {
+			method: "GET", 
+			mode: "no-cors",
+			referrerPolicy: "no-referrer",
+			headers: {
+				"Content-Type": "application/json",
+			}
+		});
+	const data = await response.json();
+	return data;
+	}
+	
+	async getBooks() {
+		const response = await fetch(`${this.#API}/books`, {
+			method: "GET", 
+			mode: "no-cors",
+			referrerPolicy: "no-referrer",
+			headers: {
+				"Content-Type": "application/json",
+			}
+		});
+	const data = await response.json();
+	return data;
+	}
+
 }
 
-async function getBooks() {
-	const response = await connAPI.get("/books");
-	return response.data;
-}
 
-export {
-	getRandomVerse,
-	getBooks
-};
+new BibleServices("nvi").getVerseOfDay().then(e => console.log(e))
+
+
+export default BibleServices;
