a928c78ade348b37d7e259ae754d6dcaeb2fc47a
Richke2005 <richardke18@gmail.com>
2024-04-30 11:36:22 -0300

    change the import and export to class BibleServices

--- src/infra/services/index.js
+++ src/infra/services/index.js
@@ -1,9 +1,8 @@
-import { getBooks, getRandomVerse } from "./bible_services";
+import BibleServices from "./bible_services";
 import { searchPhoto, curatedPhoto } from "./pexels_services";
 
 export {
-	getBooks,
-	getRandomVerse,
+	BibleServices,
 	searchPhoto,
 	curatedPhoto
 };
