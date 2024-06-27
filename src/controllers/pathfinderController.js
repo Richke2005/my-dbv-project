3da85f33a54c3ffaf5a342b57f61e2688a1032ea
Richke2005 <richardke18@gmail.com>
2024-06-27 15:40:50 -0300

    add pathfinder controller

--- src/controllers/pathfinderController.js
+++ src/controllers/pathfinderController.js
@@ -7,6 +7,18 @@ class PathfinderController extends Controller {
     constructor(){
         super(pathfinderService);
     }
+
+    async getPathfinderParents(req, res){
+        try{
+            const { id } = req.params;
+            const pathfindersParents = await pathfinderService.getParentsByPathfinderId(id);
+            if(!pathfindersParents)
+                return res.status(404).send({message: "Pathfinder do not exists"});
+            return res.status(200).send(pathfindersParents);
+        }catch(error){
+            return res.status(500).send({message: error.message});
+        }
+    }
 }
 
 module.exports = PathfinderController;
