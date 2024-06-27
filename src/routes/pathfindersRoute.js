96714ab36317917becce4fdbee757ca360606006
Richke2005 <richardke18@gmail.com>
2024-06-27 15:41:14 -0300

    update routes of pathfinder with the changes

--- src/routes/pathfindersRoute.js
+++ src/routes/pathfindersRoute.js
@@ -6,6 +6,8 @@ const router = Router();
 router
 .get('/pathfinders', (req, res) => pathfinderController.getAll(req, res))
 .get('/pathfinders/:id', (req, res) => pathfinderController.getById(req, res))
+.get('/pathfinders/:id/parents', (req, res) => pathfinderController.getPathfinderParents(req, res))
+.post('/pathfinders', (req, res) => pathfinderController.postData(req, res))
 .put('/pathfinders/:id', (req, res) => pathfinderController.updateData(req, res));
 
 
